8c016e9669ff0b240d1f48f48e0baf9ae078fe02
Rafael Martinez <30578302+rmartins87@users.noreply.github.com>
2017-11-07 17:39:06 -0200

    Conversor Code

---
+++ converte.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  6 17:39:18 2017
+
+@author: rmartins
+"""
+metros=0
+valor=0
+metros=float(input('entre com o valor em metros: '))
+valor=metros*100
+print('o valor de ', metros, 'metros, em centimetros eh: ',valor)
